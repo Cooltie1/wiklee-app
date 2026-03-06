c8a47cd305db5baaa5fa58cb9f09f7528fa5c53a
Scott Olson <scottolsonofficial@gmail.com>
2026-03-06 13:28:07 -0800

    Fix toast width and place dismiss icon inside toast

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -8,16 +8,20 @@ const Toaster = ({ ...props }: ToasterProps) => {
       className="toaster group"
       closeButton
       toastOptions={{
+        style: {
+          width: "fit-content",
+          minWidth: "0",
+        },
         classNames: {
           toast:
-            "group toast w-fit group-[.toaster]:border-zinc-200 group-[.toaster]:bg-white group-[.toaster]:text-zinc-900 group-[.toaster]:shadow-lg",
+            "group toast relative !w-fit !min-w-0 max-w-[calc(100vw-2rem)] pr-9 group-[.toaster]:border-zinc-200 group-[.toaster]:bg-white group-[.toaster]:text-zinc-900 group-[.toaster]:shadow-lg",
           description: "group-[.toast]:text-zinc-600",
           actionButton:
             "group-[.toast]:bg-zinc-900 group-[.toast]:text-zinc-50",
           cancelButton:
             "group-[.toast]:bg-zinc-100 group-[.toast]:text-zinc-500",
           closeButton:
-            "group-[.toast]:border-zinc-200 group-[.toast]:bg-white group-[.toast]:text-zinc-500 group-[.toast]:hover:bg-zinc-100",
+            "group-[.toast]:absolute group-[.toast]:right-2 group-[.toast]:top-1/2 group-[.toast]:-translate-y-1/2 group-[.toast]:border-zinc-200 group-[.toast]:bg-white group-[.toast]:text-zinc-500 group-[.toast]:hover:bg-zinc-100",
         },
       }}
       {...props}
