84b170dfb03de901ca986984e5207e348413c7bb
Scott Olson <scottolsonofficial@gmail.com>
2026-03-06 13:21:28 -0800

    Add dismiss X and fit-content width for toasts

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -6,15 +6,18 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       className="toaster group"
+      closeButton
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:border-zinc-200 group-[.toaster]:bg-white group-[.toaster]:text-zinc-900 group-[.toaster]:shadow-lg",
+            "group toast w-fit group-[.toaster]:border-zinc-200 group-[.toaster]:bg-white group-[.toaster]:text-zinc-900 group-[.toaster]:shadow-lg",
           description: "group-[.toast]:text-zinc-600",
           actionButton:
             "group-[.toast]:bg-zinc-900 group-[.toast]:text-zinc-50",
           cancelButton:
             "group-[.toast]:bg-zinc-100 group-[.toast]:text-zinc-500",
+          closeButton:
+            "group-[.toast]:border-zinc-200 group-[.toast]:bg-white group-[.toast]:text-zinc-500 group-[.toast]:hover:bg-zinc-100",
         },
       }}
       {...props}
