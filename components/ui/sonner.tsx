f93110ff4eca0020b2fb4eb9368654061ddf84e5
Scott Olson <scottolsonofficial@gmail.com>
2026-03-06 13:46:53 -0800

    Move toast close icon to top-right and restore base positioning

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -14,14 +14,14 @@ const Toaster = ({ ...props }: ToasterProps) => {
         },
         classNames: {
           toast:
-            "group toast relative !w-fit !min-w-0 max-w-[calc(100vw-2rem)] pr-9 group-[.toaster]:border-zinc-200 group-[.toaster]:bg-white group-[.toaster]:text-zinc-900 group-[.toaster]:shadow-lg",
+            "group toast !w-fit !min-w-0 max-w-[calc(100vw-2rem)] group-[.toaster]:border-zinc-200 group-[.toaster]:bg-white group-[.toaster]:text-zinc-900 group-[.toaster]:shadow-lg",
           description: "group-[.toast]:text-zinc-600",
           actionButton:
             "group-[.toast]:bg-zinc-900 group-[.toast]:text-zinc-50",
           cancelButton:
             "group-[.toast]:bg-zinc-100 group-[.toast]:text-zinc-500",
           closeButton:
-            "group-[.toast]:absolute group-[.toast]:right-2 group-[.toast]:top-1/2 group-[.toast]:-translate-y-1/2 group-[.toast]:border-zinc-200 group-[.toast]:bg-white group-[.toast]:text-zinc-500 group-[.toast]:hover:bg-zinc-100",
+            "group-[.toast]:right-2 group-[.toast]:top-2 group-[.toast]:border-zinc-200 group-[.toast]:bg-white group-[.toast]:text-zinc-500 group-[.toast]:hover:bg-zinc-100",
         },
       }}
       {...props}
